ff62823335edd1bd8aebab808e839b9e8c931ee6
jcseth <133253554+jcseth@users.noreply.github.com>
2025-06-25 17:58:56 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,11 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        primary: ['Inter', ...fontFamily.sans],
-      },
-      colors: {
-        primary: {
-          400: '#00E0F3',
-          500: '#00c4fd',
-        },
-        dark: '#333333',
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
-};
+  plugins: [],
+}
