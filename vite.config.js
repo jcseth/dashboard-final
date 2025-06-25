396acbcf48599969efd976b4f87e6e9705c41402
jcseth <133253554+jcseth@users.noreply.github.com>
2025-06-25 17:45:47 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-const path = require('path');
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // Usamos 'plugin-react' que es el correcto
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
-  },
-  plugins: [reactRefresh()],
-});
+  plugins: [react()]
+})
